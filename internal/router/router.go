efa585c2b9ad5ead836632cbd4842dddc6d882cf
doguhanniltextra <doguhangithub@gmail.com>
2025-09-13 12:27:24 +0300

    feat(internal/router): admin router added
    - Get endpoint to get all users

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,19 +2,20 @@ package router
 
 import (
 	"managify/internal/handler"
+	"managify/internal/middleware"
 	"managify/internal/validation"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Router(app *fiber.App) {
-
 	Routers(app)
 
 }
 
 func Routers(app *fiber.App) {
 	RouterUser(app)
+	RouterAdmin(app)
 }
 
 func RouterUser(app *fiber.App) {
@@ -22,4 +23,10 @@ func RouterUser(app *fiber.App) {
 
 	api.Post("/register", validation.CreateRegisterValidator, handler.CreateRegisterHandler)
 	api.Post("/auth", validation.AuthValidator, handler.LoginHandler)
+}
+
+func RouterAdmin(app *fiber.App) {
+	api := app.Group("/admin")
+
+	api.Get("/get-users", middleware.AuthMiddleware, middleware.AdminMiddleware, handler.GetUsersHandler)
 }
