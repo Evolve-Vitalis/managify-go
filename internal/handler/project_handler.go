722b8363aea18bc77b043752882de8593f26946c
doguhanniltextra <doguhangithub@gmail.com>
2025-09-13 16:50:28 +0300

    feat(internal/handler): DeleteProjectById added

--- internal/handler/project_handler.go
+++ internal/handler/project_handler.go
@@ -5,6 +5,7 @@ import (
 	"managify/models"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateProjectHandler(c *fiber.Ctx) error {
@@ -38,4 +39,34 @@ func CreateProjectHandler(c *fiber.Ctx) error {
 		"project": res,
 	})
 
+}
+
+func DeleteProjectHandler(c *fiber.Ctx) error {
+	userVal := c.Locals("user")
+	user, ok := userVal.(*models.User)
+	if !ok {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid token",
+		})
+	}
+
+	idStr := c.Params("id")
+	objID, err := primitive.ObjectIDFromHex(idStr)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid project ID",
+			"error":   err.Error(),
+		})
+	}
+
+	err = service.GetProjectService().DeleteProjectById(objID, user)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Project deleted successfully",
+	})
 }
