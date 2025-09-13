24f6ec078046d3152adeaf600a08d49b3da16a50
doguhanniltextra <doguhangithub@gmail.com>
2025-09-13 18:43:30 +0300

    feat(models): status model added

---
+++ models/status.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Status struct {
+	ID        primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	ProjectID primitive.ObjectID   `bson:"project_id" json:"project_id"`
+	CreatorID primitive.ObjectID   `bson:"creator_id" json:"creator_id"`
+	Name      string               `bson:"name" json:"name"`
+	IssueIDs  []primitive.ObjectID `bson:"issues,omitempty" json:"issues"`
+	CreatedAt time.Time            `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time            `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+}
