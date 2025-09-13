724d204e89caa3f4363d326fab4fbce724093f63
doguhanniltextra <doguhangithub@gmail.com>
2025-09-13 14:48:08 +0300

    feat(internal/service): DeleteUserById and GetUserById methods implemented

--- internal/service/admin.go
+++ internal/service/admin.go
@@ -2,12 +2,15 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"managify/database"
 	"managify/models"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -41,4 +44,67 @@ func (s *UserService) GetAllUsers() ([]models.User, error) {
 	}
 
 	return users, nil
+}
+
+func (s *UserService) GetUserById(id string) (*models.User, error) {
+	var log = logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+
+	// String'i ObjectID'ye çevir
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id: %v", err)
+	}
+
+	collection := database.DB.Collection(s.Collection)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	opts := options.FindOne().SetProjection(bson.M{"password": 0})
+
+	var user models.User
+	err = collection.FindOne(ctx, bson.M{"_id": objID}, opts).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s *UserService) DeleteUserById(id string) (*mongo.DeleteResult, error) {
+	log := logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+
+	// String -> ObjectID
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid ObjectID format: %v", err)
+	}
+
+	collection := database.DB.Collection(s.Collection)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := collection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
+		log.WithError(err).Error("failed to delete user")
+		return nil, fmt.Errorf("failed to delete user: %v", err)
+	}
+
+	if res.DeletedCount == 0 {
+		log.Warnf("user not found with id: %s", id)
+		return res, fmt.Errorf("user not found")
+	}
+
+	log.Infof("user deleted successfully: %s", id)
+	return res, nil
 }
