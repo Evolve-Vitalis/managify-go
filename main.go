8e5d78422cb24b350fbafcfc68876e8e7d59a82a
doguhanniltextra <doguhangithub@gmail.com>
2025-09-17 14:17:05 +0300

    feat: ApiLimiter added

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"managify/constant"
 	"managify/database"
 	"managify/internal/middleware"
 	"managify/internal/router"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
@@ -23,7 +27,29 @@ func main() {
 	}
 
 	app := fiber.New()
+
+	apiLimiter(app)
+
 	router.Routers(app)
 	app.Listen(os.Getenv("PORT"))
 
+}
+
+func apiLimiter(app *fiber.App) {
+
+	apiMaxLimiter, _ := strconv.Atoi(os.Getenv("API_MAX_LIMITER"))
+	expirationSec, _ := strconv.Atoi(os.Getenv("RATE_LIMIT_EXPIRATION"))
+
+	app.Use(limiter.New(limiter.Config{
+		Max:        apiMaxLimiter,
+		Expiration: time.Duration(expirationSec) * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"error": constant.ErrTooManyRequests,
+			})
+		},
+	}))
 }
