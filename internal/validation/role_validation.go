9b3e61beeac54c63cfc8a08399837de827226c4a
doguhanniltextra <doguhangithub@gmail.com>
2025-09-17 15:54:15 +0300

    feat(internal/validation): role validation added

---
+++ internal/validation/role_validation.go
@@ -0,0 +1,39 @@
+package validation
+
+import (
+	"managify/models"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+)
+
+func CreateRoleValidator(c *fiber.Ctx) error {
+
+	log := logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+	log.SetLevel(logrus.InfoLevel)
+
+	var role models.Role
+
+	if err := c.BodyParser(&role); err != nil {
+		log.WithError(err).Error("Failed to parse request body")
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid request body",
+			"error":   err.Error(),
+		})
+	}
+
+	log.Debugf("Parsed request body: %+v", role)
+
+	if role.RoleName == "" {
+		log.Error("Role name is required")
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Role name is required",
+		})
+	}
+
+	return c.Next()
+}
