5256c74bf10123e7708f636fc6c61c6027cffd37
doguhanniltextra <doguhangithub@gmail.com>
2025-09-17 19:17:21 +0300

    feat(models): PriorityType and StatusType added

--- models/issue.go
+++ models/issue.go
@@ -2,15 +2,34 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type PriorityType string
+type StatusType string
+
+const (
+	Default  PriorityType = "DEFAULT"
+	Medium   PriorityType = "MEDIUM"
+	High     PriorityType = "HIGH"
+	Urgent   PriorityType = "URGENT"
+	Critical PriorityType = "CRITICAL"
+)
+
+const (
+	TODO        StatusType = "TODO"
+	IN_PROGRESS StatusType = "IN_PROGRESS"
+	REVIEW      StatusType = "REVIEW"
+	DONE        StatusType = "DONE"
+	BLOCKED     StatusType = "BLOCKED"
+)
+
 type Issue struct {
 	ID          primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
 	Title       string               `bson:"title" json:"title"`
 	Description string               `bson:"description" json:"description"`
-	Status      string               `bson:"status" json:"status"`
+	Status      StatusType           `bson:"status" json:"status"`
 	ProjectID   primitive.ObjectID   `bson:"project_id,omitempty" json:"project_id"`
-	Priority    string               `bson:"priority" json:"priority"`
+	Priority    PriorityType         `bson:"priority" json:"priority"`
 	DueDate     string               `bson:"due_date,omitempty" json:"due_date"`
 	Tags        []string             `bson:"tags,omitempty" json:"tags"`
-	AssignedID  primitive.ObjectID   `bson:"assigned_id,omitempty" json:"assigned_id"`
+	StatusID    primitive.ObjectID   `bson:"assigned_id,omitempty" json:"status_id"`
 	CommentIDs  []primitive.ObjectID `bson:"comments,omitempty" json:"-"`
 }
