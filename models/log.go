2d261afc82ab6065fd3676446b71e49060d5c0eb
doguhanniltextra <doguhangithub@gmail.com>
2025-09-23 19:07:43 +0300

    feat(models): ProjectLog models added

---
+++ models/log.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ProjectLog MongoDB'deki log belgelerini temsil eder
+type ProjectLog struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ProjectID string             `bson:"project_id" json:"project_id"`
+	UserID    string             `bson:"user_id" json:"user_id"`
+	Message   string             `bson:"message" json:"message"`
+	Timestamp time.Time          `bson:"timestamp" json:"timestamp"`
+}
