bb31282489392977ea99dea5daaeeb28ac4802e0
doguhanniltextra <doguhangithub@gmail.com>
2025-09-30 21:23:09 +0300

    feat(constants): urls added

---
+++ managify-frontend/src/constants/urls.jsx
@@ -0,0 +1,8 @@
+export const BASE = "http://localhost:8080/"
+export const VERSION = "v1/"
+
+
+export const REGISTER = "users/register"
+export const LOGIN = "users/auth"
+
+export const CREATE_PROJECT="project/create-project"
