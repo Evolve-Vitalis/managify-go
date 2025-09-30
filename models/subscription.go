226404d360a0fbcc5691d7ca2936aabebae1f6c5
doguhanniltextra <doguhangithub@gmail.com>
2025-09-30 17:42:35 +0300

    feat(models): subscription model added

--- models/subscription.go
+++ models/subscription.go
@@ -1,6 +1,10 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type PlanType string
 
@@ -12,9 +16,9 @@ const (
 
 type Subscription struct {
 	ID                    primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	SubscriptionStartDate string             `bson:"subscription_start_date" json:"subscription_start_date"`
-	SubscriptionEndDate   string             `bson:"subscription_end_date" json:"subscription_end_date"`
+	SubscriptionStartDate time.Time          `bson:"subscription_start_date" json:"subscription_start_date"`
+	SubscriptionEndDate   time.Time          `bson:"subscription_end_date" json:"subscription_end_date"`
 	PlanType              PlanType           `bson:"plan_type" json:"plan_type"`
-	IsValid               bool               `bson:"is_valid" json:"is_valid"`
-	UserID                primitive.ObjectID `bson:"user_id,omitempty" json:"-"` // User reference
+	IsValid               bool               `bson:"is_valid" json:"-"`
+	UserID                primitive.ObjectID `bson:"user_id,omitempty" json:"-"`
 }
