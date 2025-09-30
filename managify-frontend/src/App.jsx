4b8f20007d19286aa40c1bedff5d3b51781e900a
doguhanniltextra <doguhangithub@gmail.com>
2025-09-30 21:23:50 +0300

    feat: App added
    - AuthProvider
    - RouterComponent
    - Routes

---
+++ managify-frontend/src/App.jsx
@@ -0,0 +1,72 @@
+import { BrowserRouter as Router, HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+import ManagifyLandingPage from "./components/main/home";
+import CreateProject from "./components/project/CreateProject";
+import PlanCards from "./components/plan/PlanCards";
+
+import { AuthProvider } from "./content/AuthContent";
+import ProtectedRoute from "./content/ProtectedRoute";
+import PublicRoute from "./content/PublicRoute";
+
+import { Toaster } from "react-hot-toast";
+
+const isElectron = window?.process?.versions?.electron;
+
+export default function App() {
+  const RouterComponent = isElectron ? HashRouter : BrowserRouter;
+
+  return (
+    <AuthProvider>
+      <RouterComponent>
+        <Routes>
+          <Route path="/" element={<ManagifyLandingPage />} />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-project"
+            element={
+              <ProtectedRoute>
+                <CreateProject />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/plans"
+            element={
+              <ProtectedRoute>
+                <PlanCards />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </RouterComponent>
+
+      <Toaster position="top-right" reverseOrder={false} />
+    </AuthProvider>
+  );
+}
