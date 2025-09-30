59b66f625d1f2a2515567882534b2c7d447bbad1
doguhanniltextra <doguhangithub@gmail.com>
2025-09-30 17:40:28 +0300

    feat(internal/handler): get user by id handler method implemented

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -5,6 +5,7 @@ import (
 	"managify/dto/request"
 	"managify/internal/service"
 	"managify/models"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -28,10 +29,32 @@ func CreateRegisterHandler(c *fiber.Ctx) error {
 		})
 	}
 
+	subscriptionStartDate := time.Now()
+	subscriptionEndDate := time.Now()
+	planType := models.PlanBasic
+	isValid := true
+
+	subscriptionMethod := models.Subscription{
+
+		SubscriptionStartDate: subscriptionStartDate,
+		SubscriptionEndDate:   subscriptionEndDate,
+		PlanType:              planType,
+		IsValid:               isValid,
+		UserID:                user.ID,
+	}
+	subscription, err := service.GetSubscriptionService().CreateSubscription(&subscriptionMethod)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": constant.ErrInternalServer,
+			"error":   err.Error(),
+		})
+	}
+
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"message":   constant.SuccessCreated,
-		"token":     token,
-		"userEmail": createdUser.Email,
+		"message":      constant.SuccessCreated,
+		"token":        token,
+		"userEmail":    createdUser.Email,
+		"subscription": subscription.PlanType,
 	})
 }
 func LoginHandler(c *fiber.Ctx) error {
@@ -56,5 +79,46 @@ func LoginHandler(c *fiber.Ctx) error {
 		"email":   res.Email,
 		"name":    res.FullName,
 		"token":   res.Token,
+	})
+}
+
+func GetUserByIdHandler(c *fiber.Ctx) error {
+	userIDHex := c.Params("id")
+
+	user, err := service.GetUserService().GetUserById(userIDHex)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": constant.ErrInternalServer,
+		})
+	}
+	if user == nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": constant.ErrNotFound,
+		})
+	}
+
+	project, err := service.GetProjectService().GetProjectsByUserId(userIDHex)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": constant.ErrInternalServer,
+		})
+	}
+
+	sub, err := service.GetSubscriptionService().GetByUserId(userIDHex)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": constant.ErrInternalServer,
+		})
+	}
+
+	data := fiber.Map{
+		"user":         user,
+		"project":      project,
+		"subscription": sub,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": constant.SuccessOperation,
+		"data":    data,
 	})
 }
