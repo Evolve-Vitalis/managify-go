22b1bc34a783bf444096240c7d5958b69d2d5ba6
doguhanniltextra <doguhangithub@gmail.com>
2025-09-30 17:42:57 +0300

    refactor(models): status added to project model

--- models/project.go
+++ models/project.go
@@ -11,4 +11,5 @@ type Project struct {
 	OwnerID     primitive.ObjectID   `bson:"owner_id,omitempty" json:"owner_id"`
 	IssueIDs    []primitive.ObjectID `bson:"issues,omitempty" json:"issues_id"`
 	TeamIDs     []primitive.ObjectID `bson:"team,omitempty" json:"teams_id"`
+	Status      string               `bson:"status" json:"status"`
 }
