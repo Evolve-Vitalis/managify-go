353aff556011f927989fc5402924929e694a8d5f
doguhanniltextra <doguhangithub@gmail.com>
2025-10-03 19:39:15 +0300

    refactor: status model refactored

--- models/status.go
+++ models/status.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Status struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	ProjectID primitive.ObjectID `bson:"project_id" json:"project_id"`
-	CreatorID primitive.ObjectID `bson:"creator_id" json:"-"`
-	Name      string             `bson:"name" json:"name"`
-	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	ID        primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	ProjectID primitive.ObjectID   `bson:"project_id" json:"project_id"`
+	CreatorID primitive.ObjectID   `bson:"creator_id" json:"-"`
+	Name      string               `bson:"name" json:"name"`
+	IssueIDs  []primitive.ObjectID `bson:"issues" json:"issues_id"`
+	CreatedAt time.Time            `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time            `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
